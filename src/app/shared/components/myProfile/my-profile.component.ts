15ca76e31fc12be6a0f8a89b9f6633ab23854c9c
Rupayan Dey <rupayan.dey1990@gmail.com>
2020-07-20 10:47:09 +0530

    avatar upload

--- src/app/shared/components/myProfile/my-profile.component.ts
+++ src/app/shared/components/myProfile/my-profile.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import gql from 'graphql-tag';
 import {Apollo} from 'apollo-angular';
+import { Stream } from 'stream';
 
+export interface Upload {
+  filename: string;
+  mimetype: string;
+  encoding: string;
+  createReadStream: () => Stream;
+}
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
@@ -13,7 +21,9 @@ export class MyProfileComponent implements OnInit {
   public profileForm: FormGroup;
   public profileFormData: any;
   public isLoading =  false;
-  constructor(private formBuilder: FormBuilder, private apollo: Apollo) { }
+  public avatar;
+  public avatarUploadMsg = '';
+  constructor(private formBuilder: FormBuilder, private apollo: Apollo, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.isLoading = true;
@@ -126,6 +136,95 @@ export class MyProfileComponent implements OnInit {
     } catch (err) {
       this.isLoading = false;
     }
+  }
+
+  /**
+   * Upload profile image
+   */
+  public avatarUpload(event) {
+    this.avatar = event.target.files[0] as File;
+    console.log('excelFileData ==>', this.avatar);
+    const type = /(\.jpg|\.jpeg|\.png)$/i;
+    this.avatarUploadMsg = '';
+    if (!this.avatar) {
+      this.avatarUploadMsg = 'Please choose a file';
+    }
+    // else if (!type.exec(this.avatar.name)) {
+    //   this.avatarUploadMsg = 'File extension is invalid, allowed extensions are: PDF, JPG/JPEG/PNG, DOC, ZIP/RAR';
+    // }
+    else {
+      this.uploadImageMutation();
+    }
+  }
+
+  /**
+   * public upoad image mutation
+   */
+  public uploadImageMutation() {
+    console.log('image');
+
+    // const query = `
+    //   mutation addProfilePicture($file: Upload!) {
+    //     addProfilePicture(file: $file) {
+    //           filename
+    //           mimetype
+    //           filesize
+    //       }
+    //   }
+    // `;
+
+    const operation = {
+      // tslint:disable-next-line:object-literal-key-quotes
+      'query': 'mutation ($picture: Upload!) { addProfilePicture(picture: $picture) { res }}',
+      // tslint:disable-next-line:object-literal-key-quotes
+      'variables': {
+        picture: null
+      }
+    };
+
+    const mapping = {
+      0: ['variables.picture']
+    };
+    const fd = new FormData();
+    // fd.append('file', this.avatar, this.avatar.name);
+    fd.append('operations', JSON.stringify(operation));
+    fd.append('map', JSON.stringify(mapping));
+    fd.append('0', this.avatar, this.avatar.name);
+    this.http.post('http://localhost:8000/graphql', fd).subscribe(res => {
+      console.log(res);
+    }, (err) => {
+      console.log(err);
+    })
+    // this.apollo.mutate<any>({
+    //   mutation: gql`
+    //     mutation {
+    //       addProfilePicture(picture: {
+    //         createReadStream: "${fd}",
+    //         filename: "${this.avatar.name}"
+    //       })
+    //       }
+    //     `
+    //     }).subscribe(( res ) => {
+    //       console.log(res);
+    //     }, (errors) => {
+    //       console.log(errors);
+    //     });
+
+    // this.apollo.mutate<any>({
+    //   mutation: gql`
+    //     mutation {
+    //       addProfilePicture(file: {
+    //         size: "${this.avatar.size}",
+    //         name: "${this.avatar.name}",
+    //         type: "${this.avatar.type}"
+    //       })
+    //       }
+    //     `
+    //     }).subscribe(( res ) => {
+    //       console.log(res);
+    //     }, (errors) => {
+    //       console.log(errors);
+    //     });
   }
 
 }
--- src/app/shared/components/myProfile/my-profile.module.ts
+++ src/app/shared/components/myProfile/my-profile.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MyProfileComponent } from './my-profile.component';
 import { MyProfileRoutingModule } from './my-profile.router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     MyProfileRoutingModule
   ],
   declarations: [MyProfileComponent]
