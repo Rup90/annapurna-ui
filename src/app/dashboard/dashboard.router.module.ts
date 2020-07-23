c85422e57780f18c4ff1d460e71f0370e30f347e
Rupayan Dey <rupayan.dey1990@gmail.com>
2020-07-23 09:56:07 +0530

    dashboard design change

--- src/app/dashboard/dashboard.router.module.ts
+++ src/app/dashboard/dashboard.router.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import {  HomeComponent } from '../shared/components/home/home.component';
 import { MyProfileComponent } from '../shared/components/myProfile/my-profile.component';
 const routes: Routes = [
   {
